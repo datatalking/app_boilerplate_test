27acc0eb0cc9037eea92c4ea4a51d57f88635cc8
Owner <yourdatatalking@gmail.com>
2023-12-10 17:59:06 -0800

    updated root with testing and notebooks

--- app.py
+++ app.py
@@ -18,6 +18,6 @@ def data():
 	return jsonify(get_data())
 
 
-#if __name__ == '__main__':
-#    port = int(os.environ.get('PORT', 5000))
-#    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
